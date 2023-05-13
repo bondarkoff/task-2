445cbdc3f9c8460ac53b2c6c2cd409c8ddb38c4c
bondarkoff <yehor.bondarkoff@gmail.com>
2023-05-13 17:49:34 +0300

    modal window logic

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,39 @@
+import { useState } from 'react';
 import styles from './Card.module.scss';
 
 const Card = ({ image }) => {
+    const [show, setShow] = useState(false);
+
+    const handleClick = () => {
+        setShow(!show);
+    };
+
     return (
-        <div className={styles.item}>
-            <img className={styles.image} src={image.image} alt={image.id} />
-        </div>
+        <>
+            {show && (
+                <div className={styles.modal} onClick={handleClick}>
+                    <div className={styles.overlay}>
+                        <img
+                            className={styles.modalImage}
+                            src={image.image}
+                            alt={image.id}
+                            onClick={handleClick}
+                        />
+                        <button className={styles.button} onClick={handleClick}>
+                            Close Image
+                        </button>
+                    </div>
+                </div>
+            )}
+            <div className={styles.item}>
+                <img
+                    className={styles.image}
+                    src={image.image}
+                    alt={image.id}
+                    onClick={handleClick}
+                />
+            </div>
+        </>
     );
 };
 
