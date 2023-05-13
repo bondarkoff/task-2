e132554360497a3ca807948df1ebecf16a64ee1a
bondarkoff <yehor.bondarkoff@gmail.com>
2023-05-13 17:16:11 +0300

    yarn add moment && display date and time on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Card from './components/card/Card';
-
+import moment from 'moment/moment';
 import { images } from './components/card/images.data';
 
+import Card from './components/card/Card';
+
 function App() {
+    var date = moment().format('L, h:mm a');
+
     return (
         <div className='container'>
             <div className='d-flex jcc aic flex-col'>
                 <h1 className='title'>Total images: {images.length}</h1>
+                <div>{date}</div>
             </div>
             <div className='wrapper'>
                 {images.map(image => (
