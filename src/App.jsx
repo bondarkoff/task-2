d7d97aa8941bd9fff980e59429e868c4b5e4430f
bondarkoff <yehor.bondarkoff@gmail.com>
2023-05-13 16:43:42 +0300

    yarn add css-shortcut && total images counter

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { images } from './components/card/images.data';
 function App() {
     return (
         <div className='container'>
+            <div className='d-flex jcc aic flex-col'>
+                <h1 className='title'>Total images: {images.length}</h1>
+            </div>
             <div className='wrapper'>
                 {images.map(image => (
                     <Card key={image.id} image={image} />
                 ))}
             </div>
-            <p>{images.length}</p>
         </div>
     );
 }
